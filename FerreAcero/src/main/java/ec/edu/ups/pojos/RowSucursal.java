fd618aa013af46e33aba61a04023ce25ef6c48fd
paulAAV98 <59033526+paulAAV98@users.noreply.github.com>
2022-05-20 10:23:53 -0500

    Update
    RowSucursal y continuacion de SucursalBean

--- FerreAcero/src/main/java/ec/edu/ups/pojos/RowSucursal.java
+++ FerreAcero/src/main/java/ec/edu/ups/pojos/RowSucursal.java
@@ -4,10 +4,83 @@
  */
 package ec.edu.ups.pojos;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Usuario
  */
-public class RowSucursal {
+public class RowSucursal implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    
+    private int id; 
+    private String nombre;
+    private String latitud;
+    private String longitud;
+    private boolean editable;
+
+    public RowSucursal(int id, String nombre, String latitud, String longitud) {
+        this.id = id;
+        this.nombre = nombre;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	return obj.hashCode() == this.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "RowSucursal{" + "id=" + id + ", nombre=" + nombre + ", latitud=" + latitud + ", longitud=" + longitud + '}';
+    }
+    
+    
     
 }
